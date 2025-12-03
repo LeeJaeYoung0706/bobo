996c4b52208109bcf693d941947d88d9d50e0597
leejae <148510209+LeeJaeYoung0706@users.noreply.github.com>
2025-12-03 15:22:37 +0900

    init

---
+++ human_resources_service/src/main/java/com/bobo/human_resources_service/HumanResourcesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bobo.human_resources_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesServiceApplication.class, args);
+	}
+
+}
---
+++ human_resources_service/src/test/java/com/bobo/human_resources_service/HumanResourcesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobo.human_resources_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

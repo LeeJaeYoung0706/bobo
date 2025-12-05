19495d91bb21d76f8e2c485290a18fe3f7f489be
leejae <148510209+LeeJaeYoung0706@users.noreply.github.com>
2025-12-05 15:35:41 +0900

    swagger 기본 틀 적용 및 InfoController 생성

--- human_resources_service/src/main/java/com/bobo/human_resources_service/contorllers/info/InfoControllerSwagger.java
+++ human_resources_service/src/main/java/com/bobo/human_resources_service/contorllers/info/InfoControllerSwagger.java
@@ -5,15 +5,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Tag(name = "InfoController", description = "API 정보 조회 Controller")
 public interface InfoControllerSwagger {
 
 
     @Operation(
-            summary = "get java version",
-            description = "get java version"
+            summary = "Java Version",
+            description = "자바 버전 가져오기 해당 프로젝트의 Java Version 17 로 지정"
     )
     @ApiResponses({
             @ApiResponse(
